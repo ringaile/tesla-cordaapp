a6411ebbfb1de29d3d8880ae29c148b23df18040
Ringaile Valiaugaite <ringaile@gmail.com>
2020-12-31 17:16:21 +0100

    added car contract

--- contracts/src/main/java/com/template/contracts/CarContract.java
+++ contracts/src/main/java/com/template/contracts/CarContract.java
@@ -1,10 +1,13 @@
 package com.template.contracts;
 
+import com.template.states.CarState;
 import com.template.states.TemplateState;
-import net.corda.core.contracts.CommandData;
-import net.corda.core.contracts.CommandWithParties;
-import net.corda.core.contracts.Contract;
+import net.corda.core.contracts.*;
 import net.corda.core.transactions.LedgerTransaction;
+import org.jetbrains.annotations.NotNull;
+
+import java.security.PublicKey;
+import java.util.List;
 
 import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;
 import static net.corda.core.contracts.ContractsDSL.requireThat;
@@ -19,29 +22,50 @@ public class CarContract implements Contract {
     // A transaction is valid if the verify() function of the contract of all the transaction's input and output states
     // does not throw an exception.
     @Override
-    public void verify(LedgerTransaction tx) {
+    public void verify(@NotNull LedgerTransaction tx) {
 
-        /* We can use the requireSingleCommand function to extract command data from transaction.
-         * However, it is possible to have multiple commands in a signle transaction.*/
-        final CommandWithParties<Commands> command = requireSingleCommand(tx.getCommands(), Commands.class);
-        final Commands commandData = command.getValue();
+        if(tx.getCommands().size() != 1) throw new IllegalArgumentException("There can only be one command");
 
-        if (commandData.equals(new Commands.Send())) {
-            //Retrieve the output state of the transaction
-            TemplateState output = tx.outputsOfType(TemplateState.class).get(0);
+        Command command = tx.getCommand(0);
+        CommandData commandType = command.getValue();
+        List<PublicKey> requiredSigners = command.getSigners();
 
-            //Using Corda DSL function requireThat to replicate conditions-checks
-            requireThat(require -> {
-                require.using("No inputs should be consumed when sending the Hello-World message.", tx.getInputStates().size() == 0);
-                require.using("The message must be Hello-World", output.getMsg().equals("Hello-World"));
-                return null;
-            });
+        if(commandType instanceof Commands.Shipment) {
+            // Shipment Rules
+
+            //Shape rules
+
+            if(tx.getInputStates().size() != 0) {
+                throw new IllegalArgumentException("There cannot be input states");
+            }
+
+            if(tx.getOutputStates().size() != 1) {
+                throw new IllegalArgumentException("Only one car can be shipped at a time");
+            }
+            //Content rules
+
+            ContractState outputState = tx.getOutput(0);
+            if(!(outputState instanceof CarState)) {
+                throw new IllegalArgumentException("Output has to be of CarState");
+            }
+
+            CarState carState = (CarState) outputState;
+            if(!(carState.getModel().equals("Cybertruck"))) {
+                throw new IllegalArgumentException("This is not a Cybertruck");
+            }
+
+            //Signer rules
+            PublicKey manufacturerKey = carState.getManufacturer().getOwningKey();
+            if (!(requiredSigners.contains(manufacturerKey))) {
+                throw new IllegalArgumentException("Manufacturer must sign the transaction");
+            }
+
         }
     }
 
     // Used to indicate the transaction's intent.
     public interface Commands extends CommandData {
         //In our hello-world app, We will only have one command.
-        class Send implements Commands {}
+        class Shipment implements Commands {}
     }
 }
